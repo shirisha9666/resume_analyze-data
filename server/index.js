72b431bda7c86cb4b5602f1ab19128c0289fb144
shirisha9666 <kommushirisha9666@gmail.com>
2025-05-09 00:15:39 +0530

    test-10

--- server/index.js
+++ server/index.js
@@ -22,7 +22,7 @@ app.use("/api/resume",resumeRoute)
 app.use(express.static(path.join(__dirname, '../frontend/build')));
 
 app.all('/{*any}', (req, res) => {
-  // res.sendFile(path.join(__dirname, '../frontend/build', 'index.html'));
+ 
   res.sendFile(path.join(__dirname, "../frontend/build/index.html"));
 });
 
