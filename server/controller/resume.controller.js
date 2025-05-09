70696ed427f3080645137567332c2070cb2264f7
shirisha9666 <kommushirisha9666@gmail.com>
2025-05-09 21:42:51 +0530

    test-29

--- server/controller/resume.controller.js
+++ server/controller/resume.controller.js
@@ -8,19 +8,7 @@ import cloudinary from "../lib/cloudinary.js";
 
 const analyzeResume = async (resumeText, jobDescription) => {
   try {
-    //         console.log("resumeText, jobDescription", resumeText, jobDescription)
-    //  const resumePath = path.resolve('D:/MERN Stack/resume_analyze/uploads', resumeText);
-    //         const resumeBuffer = fs.readFileSync(resumePath)
-    //         const pdfData = await pdf(resumeBuffer)
-    //         const resumeTextRead = pdfData.text
-    //         console.log("resumeTextRead", resumeTextRead)
-    // send to the LLM
-    // const response = await axios.get(resumeText, {
-    //   responseType: "arraybuffer",
-    // });
-    // const resumeBuffer = response.data;
-    // const pdfData = await pdf(resumeBuffer);
-    // const resumeTextRead = pdfData.data;
+ 
 
     const result = await sendToLLM({
       resumeText: resumeText,
@@ -41,15 +29,13 @@ export const uploadResume = async (req, res) => {
       return res.status(400).json({ message: "Resume file is required" });
     }
      const pdfData = await pdf(fs.readFileSync(resumeFile.path));
-    const resumeTextRead = pdfData.text;
+
     const cloudinaryResult = await cloudinary.uploader.upload(resumeFile.path, {
       resource_type: "raw",
       folder: "resumes",
          access_mode: "public",
     });
     const cloudinaryUrl = cloudinaryResult.secure_url;
-    // const resumePath = req.files?.resume?.[0]?.path;
-    // const resumPathreplace = resumePath.replace(/\\/g, '/');
 
     console.log("req.files:", cloudinaryUrl);
     const newUser = await Resume.create({
