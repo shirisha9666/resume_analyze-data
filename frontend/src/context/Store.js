e3574977b491bd93decc97edea025a4202f78b72
shirisha9666 <kommushirisha9666@gmail.com>
2025-05-09 15:26:02 +0530

    test-23

--- frontend/src/context/Store.js
+++ frontend/src/context/Store.js
@@ -6,7 +6,8 @@ const AppContext = createContext();
 export const AppProvider = ({ children }) => {
 const [loading,setLoading]=useState(false)
 
-  const BASE_URL =`${process.env.REACT_APP_API_URL}/api/resume/upload`;
+  // const BASE_URL =`${process.env.REACT_APP_API_URL}/api/resume/upload`;
+  const BASE_URL = "https://resume-analyze-data-4.onrender.com/api/resume/upload";
 
   const uploadresume = async (file, jobDescriptionText) => {
     try {
